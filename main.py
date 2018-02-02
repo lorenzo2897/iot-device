346f63c68bedd73ae7369248c3afec439443e120
Marco Lavalle <ml7715@ic.ac.uk>
2018-02-02 00:16:28 +0000

    MQTT library implemented and tested, sample callback functions provided in main

--- main.py
+++ main.py
@@ -1,21 +1,38 @@
-from machine import Pin
-from sensors.rgb import RGB
-from sensors.temperature import Temperature
-import utime
+from mqtt.MQTTembedded import IotClient
+import uasyncio as asyncio
+import network
 
-rgb = RGB(sda=Pin(4), scl=Pin(5), led=Pin(16, Pin.OUT))
-temp = Temperature(sda=Pin(4), scl=Pin(5))
+#Sample functions, use same structure
+import json
+def stats():
+	data = '{"name": "John Smith", "hometown": {"name": "New York", "id": 123}}'
+	x = json.loads(data, object_hook=lambda d: namedtuple('X', d.keys())(*d.values()))
+	return x
 
+async def make_tea():
+	while True:
+		print("started")
+		await asyncio.sleep(5)
+
+def abort():
+	print("aborted")
+
+def update_settings(settings):
+	print(json.dumps(settings))
+
+
+#connect to hotspot
+def activate_network():
+	ap_if = network.WLAN(network.AP_IF)
+	ap_if.active(False)
+
+	sta_if = network.WLAN(network.STA_IF)
+	sta_if.active(True)
+	sta_if.connect('OnePlus 3', 'a10101010a')
 
 def main():
-	rgb.begin(50, 4)
+	client = IotClient("silvestri.io", stats, make_tea, abort, update_settings)
+	client.begin()
 
-	while 1:
-		# print(rgb.read_color())  # end='         \r'
-
-		utime.sleep(1)
-
-
-if __name__ == '__main__':
-	# main()
-	pass
+#if __name__ == '__main__':
+#	main()
--- mqtt/MQTTembedded.py
+++ mqtt/MQTTembedded.py
@@ -1,19 +1,50 @@
 from umqtt.simple import MQTTClient
+import uasyncio as asyncio
+import ubinascii
+import machine
+import json
+
 
 class IotClient:
-    #add stats function
-    def __init__(self, ip, port, stats_callback):
-        self.client = MQTTClient()
-        self.client.connect(ip, port, 60)
-        self.client.loop_start()
+	def __init__(self, ip, stats, make_tea, abort, update_settings):
+		self.client = MQTTClient(ubinascii.hexlify(machine.unique_id()), ip)
+		self.client.set_callback(self.callback)
+		self.client.connect()
+		self.client.subscribe(b"commands")
+		self.client.subscribe(b"set")
+		self.stats = stats
+		self.make_tea = make_tea
+		self.abort = abort
+		self.update_settings = update_settings
+		self.loop = asyncio.get_event_loop()
 
-    def __exit__(self, exc_type, exc_value, traceback):
-        self.client.loop_stop()
-        self.client.disconnect()
+	def __exit__(self, exc_type, exc_value, traceback):
+		self.client.loop_stop()
+		self.client.disconnect()
 
-	#content JSON string
-    def push_notification(self, content):
-    def callback(self, )
-    def test(self, topic = 'Debug', message = 'Hello'):
-        self.client.publish(topic, message)
-    
+	def begin(self):
+		self.loop.create_task(self.process_msgs())
+		self.loop.run_forever()
+
+	def push_notification(self, content):
+		self.client.publish(b"push", content.encode())
+
+	async def process_msgs(self):
+		while True:
+			self.client.check_msg()
+			await asyncio.sleep(5)
+
+	def callback(self, topic, msg):
+		if topic == b"commands":
+			if msg == b"make_tea":
+				self.loop.create_task(self.make_tea())
+			elif msg == b"abort":
+				self.abort()
+			elif msg == b"stats":
+				self.client.publish(b"stats", json.dumps(self.stats()).encode())
+		elif topic == b"set":
+			settings = json.loads(msg.decode())
+			self.update_settings(settings)
+
+	def test(self, topic='Debug', message='Hello'):
+		self.client.publish(topic, message)
