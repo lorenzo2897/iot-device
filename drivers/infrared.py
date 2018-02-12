90eecf34fb8da37a865e6c50043cba067b23fa72
Lorenzo Silvestri <code@silvestrix.org>
2018-02-12 14:54:20 +0000

    added IR temperature sensor driver

---
+++ drivers/infrared.py
@@ -0,0 +1,44 @@
+from machine import I2C
+
+
+class Infrared:
+	BASE_I2C_ADDRESS = 0x40
+	__i2c_address = BASE_I2C_ADDRESS
+
+	def __init__(self, sda, scl, addr=BASE_I2C_ADDRESS, samplerate=16):
+		# initialise the I2C port
+		self.__i2c = I2C(scl=scl, sda=sda, freq=100000)
+		self.__i2c_address = addr
+		self.set_rate(samplerate)
+
+	def __read16(self, reg):
+		# tells the chip where to read
+		command = reg & 0xFF
+		self.__i2c.writeto(self.__i2c_address, bytearray([command]))
+		# read a byte
+		data = self.__i2c.readfrom(self.__i2c_address, 2)
+		return int.from_bytes(data, 'big')
+
+	def __write16(self, reg, value):
+		command = reg & 0xFF  # tells the chip where to write
+		valueHi = (value >> 8) & 0xFF  # tells the chip the value to write
+		valueLo = value & 0xFF
+		self.__i2c.writeto(self.__i2c_address, bytearray([command, valueHi, valueLo]))
+
+	def set_rate(self, rate):
+		TMP007_CONFIG = 0x02
+		TMP007_CFG_MODEON = 0x1000
+		TMP007_CFG_SAMPLE = {1: 0x0000, 2: 0x0200, 4: 0x0400, 8: 0x0600, 16: 0x0800}
+		self.__write16(TMP007_CONFIG, TMP007_CFG_MODEON | TMP007_CFG_SAMPLE[rate])  # TODO check datasheet
+
+	def get_die_temperature(self):
+		TMP007_TDIE = 0x01
+		raw = self.__read16(TMP007_TDIE) >> 2
+		return raw * 0.03125
+
+	def get_obj_temperature(self):
+		TMP007_TOBJ = 0x03
+		raw = self.__read16(TMP007_TOBJ)
+		if raw & 0x1:
+			return 0
+		return (raw >> 2) * 0.03125
