c42d428afa19c5bd438754e72606e4adf83e5345
Lorenzo Silvestri <code@silvestrix.org>
2018-02-14 19:14:17 +0000

    implemented actual sensing

--- tea.py
+++ tea.py
@@ -1,4 +1,5 @@
 from machine import Pin
+import utime
 from drivers.servo import Servo
 from drivers.rgb import RGB
 from drivers.infrared import Infrared
@@ -9,8 +10,8 @@ import uasyncio as asyncio
 class Tea:
 	send_push = None
 
-	__SERVO_LOWERED = 0.60
-	__SERVO_RAISED = 0.36
+	__SERVO_LOWERED = 0.64
+	__SERVO_RAISED = 0.40
 
 	def __init__(self):
 		self.concentration = 0.50
@@ -62,8 +63,19 @@ class Tea:
 		self.send_push(self.stats())
 
 		self.boiler.on()
-		await asyncio.sleep(4)
+		print("boiler on")
+		await asyncio.sleep(2)
+
+		while True:
+			temp = self.boiler_temp.read_temperature()
+			print("boiler temperature is", temp)
+			if temp is not None and temp > 90.0:
+				break
+			await asyncio.sleep(3)
+
 		self.boiler.off()
+		print("boiler off")
+		await asyncio.sleep(2)
 
 		# ===================== pump water into cup =====================
 		if self.state == 'aborting':
@@ -72,9 +84,13 @@ class Tea:
 		self.state = 'pumping'
 		self.send_push(self.stats())
 
+		await asyncio.sleep(2)
+		print("pump on")
 		self.pump.on()
-		await asyncio.sleep(4)
+		await asyncio.sleep(4)  # TODO calibrate to pump
 		self.pump.off()
+		print("pump off")
+		await asyncio.sleep(2)
 
 		# ======================== lower tea-bag ========================
 		if self.state == 'aborting':
@@ -93,9 +109,38 @@ class Tea:
 		self.state = 'brewing'
 		self.send_push(self.stats())
 
+		# let the tea bag settle a bit
+		await asyncio.sleep(3)
+
+		concentrations = {
+			0: (600, 30),
+			0.25: (500, 60),
+			0.5: (450, 90),
+			0.75: (400, 120),
+			1: (300, 180)
+		}
+
+		# find target concentration and timeout from strength setting
+		target_concentration = 500
+		timeout = 60
+		for key, value in concentrations.items():
+			if key - 0.05 > self.concentration:
+				target_concentration = value[0]
+				timeout = value[1]
+
+		# measure colour until good or until timeout
 		self.rgb.set_led(1)
-		await asyncio.sleep(4)
+		print("colour sensor on")
+		start_time = utime.ticks_ms()
+		while utime.ticks_ms() < start_time + 1000 * timeout:
+			await asyncio.sleep(3)
+			col = self.rgb.read_color()[0]  # TODO improve accuracy
+			print("concentration reading is", col)
+			if col < target_concentration:
+				break
+
 		self.rgb.set_led(0)
+		print("colour sensor off")
 
 		# ======================= remove tea-bag ========================
 		if self.state == 'aborting':
@@ -114,9 +159,17 @@ class Tea:
 		self.state = 'cooling'
 		self.send_push(self.stats())
 
-		await asyncio.sleep(4)
+		await asyncio.sleep(2)
+
+		while True:
+			temp = self.boiler_temp.read_temperature()
+			print("tea temperature is", temp)
+			if temp is not None and temp < self.temperature:
+				break
+			await asyncio.sleep(3)
 
 		# ==================== notify user when done ====================
+		print("Done!")
 		self.state = 'done'
 		stats = self.stats()
 		stats["notify"] = True
